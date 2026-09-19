6f40c2e45ed683f23b17c27b34308db0093a5076
popovakatua2013-jpg <popovakatua2013@yandex.ru>
2026-09-19 18:16:24 +0300

    dropdown: открытие/закрытие списка, выбор значения

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,19 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const valueElement = dropdown.querySelector('.dropdown__value');
+    const listElement = dropdown.querySelector('.dropdown__list');
+    const links = dropdown.querySelectorAll('.dropdown__link');
+
+    valueElement.addEventListener('click', () => {
+        listElement.classList.toggle('dropdown__list_active');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            valueElement.textContent = link.textContent.trim();
+            listElement.classList.remove('dropdown__list_active');
+        });
+    });
+});
